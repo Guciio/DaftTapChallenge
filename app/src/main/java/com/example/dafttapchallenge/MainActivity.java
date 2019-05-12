e09e992e53c1d7437288e40229d23aedb45cc4aa
Gutek <gucio332@gmail.com>
2019-05-12 14:29:57 +0200

    Basic counting of clicks

--- app/src/main/java/com/example/dafttapchallenge/MainActivity.java
+++ app/src/main/java/com/example/dafttapchallenge/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,10 +13,13 @@ public class MainActivity extends AppCompatActivity
 {
 	private static final long TIME_TO_START_GAME_IN_MILLIS = 3000;
 
+	public int numberOfCilcks;
+
 	private TextView timerView;
 	private Button clickButton;
 	private FloatingActionButton goToScoreButton;
 
+	private Boolean canClick;
 	private CountDownTimer mCountDownTimer;
 
 	private long timeToStart = TIME_TO_START_GAME_IN_MILLIS;
@@ -27,8 +32,22 @@ public class MainActivity extends AppCompatActivity
 
 		timerView = findViewById(R.id.timer);
 		clickButton = findViewById(R.id.clickButton);
+		clickButton.setEnabled(false);
 
 		timer();
+
+		clickButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				if(canClick)
+				{
+					numberOfCilcks++;
+					Log.i("Number of cliks",String.valueOf(numberOfCilcks));
+				}
+			}
+		});
 	}
 
 	private void game()
@@ -51,8 +70,11 @@ public class MainActivity extends AppCompatActivity
 			public void onFinish()
 			{
 				timerView.setText("GO");
+				clickButton.setEnabled(true);
+				canClick=true;
 			}
 		}.start();
+		canClick = false;
 	}
 
 	private void updateTimerView()
